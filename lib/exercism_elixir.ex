ff41f5b79037a5cd2e6cd9b53b1cc131e03689a6
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-03-15 17:15:05 -0600

    Create a new mix project

---
+++ lib/exercism_elixir.ex
@@ -0,0 +1,18 @@
+defmodule ExercismElixir do
+  @moduledoc """
+  Documentation for `ExercismElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExercismElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
