e01175797266f6c7eef5937f8194de68de071502
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-04-19 22:26:49 -0500

    Add coveralls dependency and config

--- mix.exs
+++ mix.exs
@@ -7,21 +7,30 @@ defmodule ExercismElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :credo]
+      extra_applications: [:logger, :credo, :excoveralls]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.6"}
+      {:credo, "~> 1.6"},
+      {:excoveralls, "~> 0.14.4", only: :test}
     ]
   end
 end
