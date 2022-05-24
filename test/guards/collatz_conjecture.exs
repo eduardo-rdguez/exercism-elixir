56ce75d97ebf50bb780f917a3524c5fb4380a488
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-05-23 22:48:42 -0500

    Init collatz conjecture exercise solution

---
+++ test/guards/collatz_conjecture.exs
@@ -0,0 +1,15 @@
+defmodule Guards.CollatzConjecture do
+  @moduledoc """
+  This module tries to find a number that tries to solve the collatz conjecture.
+  """
+
+  @doc """
+  calc/1 takes an integer and returns the number of steps required to get the
+  number to 1 when following the rules:
+    - if number is odd, multiply with 3 and add 1
+    - if number is even, divide by 2
+  """
+  @spec calc(input :: pos_integer()) :: non_neg_integer()
+  def calc(input) do
+  end
+end
