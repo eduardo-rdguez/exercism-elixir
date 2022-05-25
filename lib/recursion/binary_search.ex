841921be275db46bc529222e9a7b3bdf0d1b1fc0
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-05-24 23:37:01 -0500

    Init binary search exercise solution

---
+++ lib/recursion/binary_search.ex
@@ -0,0 +1,26 @@
+defmodule Recursion.BinarySearch do
+  @moduledoc """
+  This module implement a binary search algorithm.
+  """
+
+  @doc """
+    Searches for a key in the tuple using the binary search algorithm.
+    It returns :not_found if the key is not in the tuple.
+    Otherwise returns {:ok, index}.
+
+    ## Examples
+
+      iex> BinarySearch.search({}, 2)
+      :not_found
+
+      iex> BinarySearch.search({1, 3, 5}, 2)
+      :not_found
+
+      iex> BinarySearch.search({1, 3, 5}, 5)
+      {:ok, 2}
+
+  """
+  @spec search(tuple, integer) :: {:ok, integer} | :not_found
+  def search(numbers, key) do
+  end
+end
