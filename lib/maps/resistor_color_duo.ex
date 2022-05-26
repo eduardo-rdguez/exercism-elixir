51249a8e696db94199d6d1e43733b113e99235ae
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-05-25 23:10:25 -0500

    Init resistor color duo exercise solution

---
+++ lib/maps/resistor_color_duo.ex
@@ -0,0 +1,12 @@
+defmodule Maps.ResistorColorDuo do
+  @moduledoc """
+    This module indicates the values of a resistor.
+  """
+
+  @doc """
+  Calculate a resistance value from two colors
+  """
+  @spec value(colors :: [atom]) :: integer
+  def value(colors) do
+  end
+end
